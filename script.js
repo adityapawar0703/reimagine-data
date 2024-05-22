462c1a23a811101ad579644d910cb2c13891089e
adityapawar0703 <adityapawar07m@gmail.com>
2024-05-22 22:33:42 +0530

    images

---
+++ script.js
@@ -0,0 +1,16 @@
+var tl = gsap.timeline();
+tl.to("#img1",{
+    
+    y: 15,
+    x: -1030,
+    // opacity:0,
+    duration:6,
+    
+})
+
+gsap.from("#img6",{
+    y:500,
+    opacity:0,
+    duration:2,
+})
+
